c994027f54352f3ae32f884f93ecfb641cb417a1
Scott <sc4112sc@gmail.com>
2019-01-01 22:33:32 +0800

    Initial Commit

---
+++ JungleLife/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JungleLife
+//
+//  Created by 李書賢 on 2019/1/1.
+//  Copyright © 2019 Scott.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
